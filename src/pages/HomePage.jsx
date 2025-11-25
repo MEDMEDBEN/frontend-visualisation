f5235966db3b2c5e1dd5b6012f0fc97d799babf6
MEDMEDBEN <benamo.med31@gmail.com>
2025-11-25 03:48:50 +0100

    derniere modification2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,10 +6,9 @@ export default function HomePage(){
   return (
     <section className="home-page">
       <div className="home-container">
-        <h1 className="home-title">Med and Jesse — Données Déchet de L'Inde</h1>
-
+        <h1 className="home-title"><b>Med and Jess</b> Dashboard</h1>
+        <h2>Gestion et suivi de déchet en Inde</h2>
         <div className="home-card">
-          <p className="home-intro">Bienvenue — aperçu rapide des données indiennes sur la gestion des déchets. Ci-dessous, une sélection de la table de données et des notifications informatives qui tournent toutes les 3 secondes.</p>
         </div>
 
         <HomeIndianTable />
